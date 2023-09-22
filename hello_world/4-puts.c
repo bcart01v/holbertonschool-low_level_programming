9a8956ca39de32e837339e106e18094255c5c242
bcart01v <bcart0v@gmail.com>
2023-09-22 07:39:48 -0700

    My first C program

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - here is the heart of this program <3
+ * Return - Should be 0
+ */
+int main(void)
+{
+	puts("\Programming is like building a multilingual puzzle");
+	return (0);
+}
