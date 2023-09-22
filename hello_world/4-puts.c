071d6fc22eadb55827c513dddf624507902783b1
bcart01v <bcart0v@gmail.com>
2023-09-22 07:55:39 -0700

    Need to compile..

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
- * main-Will return a pre-made input
- * Return: Should be 0
+ * main-Main Function
+ * Return: It's 0
  */
 int main(void)
 {
-	puts("\Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
