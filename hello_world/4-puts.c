06436d697b769b2899b1f8abd69fc377a4eadacf
bcart01v <bcart0v@gmail.com>
2023-09-22 07:58:07 -0700

    Removed space

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * main-Main Function
  * Return: It's 0
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
