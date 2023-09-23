f9b25d1405b38405a856e0d9ad3e7a47cd4c7c8d
bcart01v <bcart0v@gmail.com>
2023-09-22 23:40:39 -0700

    Printing Numbers using the same function I guess?

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers of base 10
+ *
+ * Return: Should return 0 pending no errors
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
