cdbd19c1aa5dd790c89bf6f0ec3624b31f8ff5ac
bcart01v <bcart0v@gmail.com>
2023-09-29 13:06:54 -0700

    Project 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - Multiplies 2 numbers together
+* @a: first number
+* @b: seond number
+* Return: returns the result of the equation
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
