e2a9fe5323dc9f37a0428c1809c0509fd4e774d4
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-09-30 14:18:40 -0500

    Last update for project 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include <unistd.h>
 #include "main.h"
-/**
- * print_numbers - prints from 0 to 9
- *
- * Return: none
- */
 
+/**
+* print_numbers - prints from 0 to 9
+*
+* Return: none
+*/
 void print_numbers(void)
 {
 	int num;
+
 	for (num = 0; num < 10; num++)
 	{
 		_putchar(num + '0');
 	}
 	_putchar('\n');
-
 }
