2aa8beaffefa33adae9b17e5bf56c8ef36683b85
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-09-30 13:08:49 -0500

    Project 3, updated Main

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers 0 to 9
+ *  @i - The Character I'm using
+ * BJC 2023-09-30
+*/
+int print_numbers(void)
+{ int i;
+	for (i = 48 ; i <= 57 ; i++) 
+     {
+     _putchar(i);
+     } 
+     _putchar('\n');
+}
