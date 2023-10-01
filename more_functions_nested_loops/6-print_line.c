02eee1b051153209914101a17d7a6193eedb0891
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-01 18:46:38 -0500

    project 6 Alpha 1

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a line that the user inputs
+ * @n: the charecter I'm using
+ * bjc 2023 10 01
+*/
+
+void print_line(int)
+{
+int n;
+int i;
+	for (i = 0; i <n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('/n');
+	return(n);
+}
