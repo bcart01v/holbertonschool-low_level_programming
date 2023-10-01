d81fa64f7a4b1be018315a08bbd55db5865d0163
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-01 18:03:09 -0500

    Project 5 Alpha 1

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - just prints more numbers
+ * bjc 2023 10 01
+*/
+
+void more_numbers(void)
+{
+    int number;
+    for (number = 0; number > 15; number++)
+    {
+    printf("01234567891011121314\n");
+    }
+}
