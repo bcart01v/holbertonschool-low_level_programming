ab969f52655ce733d43499bc784cf9bed37c28a8
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-02 10:37:11 -0500

    Betty Updates

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* _strlen - returns the length of the string passed 
+* _strlen - returns the length of the string passed
 * @s: the string we are evaluating
 * Return: The length of the string
-* BJC - 2023-10-02 
+* BJC - 2023-10-02
 */
 
 int _strlen(char *s)
