ebe8262fa8df0bffdd5cf8a0126b6405ab310290
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-02 13:14:35 -0500

    Betty

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -15,10 +15,10 @@ unsigned int num = 0;
 do {
 	if (*s == '-')
 	i *= -1;
-		else if (*s >= '0' && *s <= '9')
+	else if (*s >= '0' && *s <= '9')
 	num = num * 10 + (*s - '0');
-		else if (num > 0)
+	else if (num > 0)
 	break;
 	} while (*s++);
-	return (num *i);
+	return (num * i);
 }
