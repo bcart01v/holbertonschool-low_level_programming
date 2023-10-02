6c35c16cf8a31916082ceeef82dcce5168c71f03
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-02 11:36:15 -0500

    Updated Main, created project 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_rev - prints reverse order. 
+* @s: string we are reversing
+* BJC - 2023-10-02
+*/
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\n')
+	{
+		c++;
+	}
+	for (c -=1; c >=0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
