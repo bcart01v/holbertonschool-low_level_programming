714304fb05e9a7d0746e27c6efa7b15c445a0352
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-01 19:47:33 -0500

    Project 8 Alpha

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_square - prints a square
+* @size: passed character of size that we should print
+* bjc - 2023-10-1
+*/
+
+void print_square(int size)
+{
+int counter;
+
+if (size <= 0) {
+	_putchar('\n');
+	} else {
+		for (counter = 0; counter < size; counter++) {
+			_putchar(35);
+		}
+	}
+}
