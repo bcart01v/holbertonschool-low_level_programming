aade400cae5317eb234ff2630e4470e983877260
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-02 09:58:24 -0500

    Betty Updates

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
-* swap_int - swaps 2 ints 
-* 
+* swap_int - swaps 2 ints
+* @a: first number
+* @b: second number
+* BJC - 2023-10-02
 */
 
 void swap_int (int *a, int *b)
