57af63451a4e9b402ff25f0cc6355304f1ac473b
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-04 17:08:30 -0500

    Project 1, updated main

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* *_memcpy - Copies n bytes
+* @dest: Destication
+* @src: The Source
+* @n: An Int
+* BJC - 2023-10-04
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n) 
+{
+	
+	// the memcpy function copies n bytes from memory area src, to memory area dest
+	// returns a pointer to dest. 
+	while (n--) {
+		*dest = *src;
+		dest++;
+		src++;
+	}
+return (dest);
+}
--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,5 +1,6 @@
 int main(void)
 {
 	char *_memset(char *s, char b, unsigned int n);
+	char *_memcpy(char *dest, char *src, unsigned int n);
 	return(0);
 }
