30644ac08678f93c3d2343650c7579d5af00a6c7
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-06 10:01:27 -0500

    Fixin' Betty

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -15,7 +15,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
  * the _memcpy function copies n bytes from
  * memory area to src, to memory area dest
  * returns a pointer to dest.
- * 
+ *
  * Also, the reason for OutdatedCounter is
  * the 89 standard doesn't allow compairing
  * an unsigned int to an int.
