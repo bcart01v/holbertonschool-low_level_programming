393cbcc8a34cc7c0380e3823b043652d61f46b7a
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-06 23:00:22 -0500

    Renamed variables for shorter lines

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,11 @@
 
 void print_diagsums(int *a, int size)
 {
-	int first, second, sum1 = 0, sum2 = 0;
+	int one, two, sum1 = 0, sum2 = 0;
 
-	for (first = 0; first <= (size * size); first = first + size + 1)
-		sum1 = sum1 + a[first];
-	for (second = size - 1; second <=  (size * size) - size; \
-	 second = second + size - 1)
-		sum2 = sum2 + a[second];
+	for (one = 0; one <= (size * size); one = one + size + 1)
+		sum1 = sum1 + a[one];
+	for (two = size - 1; two <=  (size * size) - size; two = two + size - 1)
+		sum2 = sum2 + a[two];
 	printf("%d, %d\n", sum1, sum2);
 }
