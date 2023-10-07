0e72cc933d5b8d99b08399398123b92f0bbfee4b
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-06 22:03:55 -0500

    Updated Main, Project 4

---
+++ functions_nested_loops/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strpbrk - Searches a string
+* @s: The String we are searching
+* @accept: The string we're comparing it too
+* Return: The results of the compairison
+* BJC - 2023-10-06
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int firstcount, secondcount;
+	char *diddlydoo;
+
+	for (firstcount = 0; s[firstcount] !='\0'; firstcount++)
+	{
+		for (secondcount = 0; accept[secondcount] !='\0' ;secondcount++)
+		{
+			if (accept[secondcount] == s[firstcount])
+			{
+				diddlydoo = &s[firstcount];
+				return (diddlydoo);
+			}
+		}
+	}
+return (0);
+}
