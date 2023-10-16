cf147bf6e32a09574280a04d7c4052c80b7d9847
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-16 10:11:40 -0500

    Fixed Factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -9,21 +9,10 @@
 
 int factorial(int n)
 	{
-		if (n != '\0')
-		{
-			if (n < 0)
-			{
-				return (-1);
-			}
-			if (n == 0)
-			{
-				return (1);
-			}
-			else
-			{
-				return (n * factorial(n - 1));
-			}
-			factorial (n + 1);
-		}
-	return (0);
+		if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
 	}
