1a4f9a85d1c2bafad94cc4395387d1fd89e4e56e
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-21 03:22:16 -0500

    Project 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - function that count the number of chars in a string
+ * @s: input string
+ * Return: lenght of a string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: pointer to a string (s1 and s2), NULL if it fails
+ */
+
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int j;
+	int len1;
+	int len2;
+	char *concat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	concat = malloc((len1 + len2 + 1) * sizeof(char));
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+
+	for (j = 0; j < len2; j++)
+		concat[j + i] = s2[j];
+
+	concat[j + i] = '\0';
+	return (concat);
+}
+
+int main (void) 
+
+{
+ char *s1 = "a";
+ char *s2 = "b";
+ char *str_concat(char *s1, char*s2);
+}
