ca352da4893d60a1fe459fc5ca62975199e21780
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-23 11:04:40 -0500

    Included a Main

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * **alloc_grid-fuction allocating two dimention and intilizing to 0
  * @width:width of the grid
@@ -7,40 +8,44 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **grid;
-	int i, j;
+    int **grid;
+    int i, j;
 
-	if (width <= 0)
-	{
-		return (NULL);
-	}
-	if (height <= 0)
-	{
-		return (NULL);
-	}
+    if (width <= 0)
+    {
+        return (NULL);
+    }
+    if (height <= 0)
+    {
+        return (NULL);
+    }
 
-	grid = (int **)malloc(height * sizeof(int *));
-	if (grid == NULL)
-	{
-		return (NULL);
-	}
+    grid = (int **)malloc(height * sizeof(int *));
+    if (grid == NULL)
+    {
+        return (NULL);
+    }
 
-	for (i = 0; i < height; i++)
-	{
-	grid[i] = (int *)malloc(width * sizeof(int));
-	if (grid[i] == NULL)
-	{
-		for (j = 0; j < i; j++)
-		{
-		free(grid[j]);
-		}
-	free(grid);
-	return (NULL);
-	}
-	for (j = 0; j < width; j++)
-	{
-		grid[i][j] = 0;
-	}
-	}
-	return (grid);
+    for (i = 0; i < height; i++)
+    {
+        grid[i] = (int *)malloc(width * sizeof(int));
+        if (grid[i] == NULL)
+        {
+            for (j = 0; j < i; j++)
+            {
+                free(grid[j]);
+            }
+            free(grid);
+            return (NULL);
+        }
+        for (j = 0; j < width; j++)
+        {
+            grid[i][j] = 0;
+        }
+    }
+    return (grid);
+}
+
+int main() {
+  return 0;
 }
