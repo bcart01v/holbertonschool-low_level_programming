9c9a70c34944e72c1f9642c5cbc21d57c7989751
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-23 12:05:30 -0500

    Betty Fixes

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -4,19 +4,17 @@
  * *malloc_checked - This function allocates memory using malloc
  * @b: the value we are checking
  * 
- * Return - Possibly none
+ * Return: Pointer
 */
 
 void *malloc_checked(unsigned int b)
 {
-    void *ptr;
+	void *ptr;
 
-    ptr = malloc(b);
-
-    if (ptr == NULL)
-    {
-        exit(98); 
-    }
-
-    return ptr;
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+	exit(98);
+	}
+	return (ptr);
 }
