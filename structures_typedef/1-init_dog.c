702a9124e8ba42c7b375c13b59109fdf58f1c012
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-25 16:13:13 -0500

    Updates

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,14 +1,20 @@
 #include "dog.h"
 #include <stdio.h>
+
 /**
- * init_dog - 
- * 
- * 
- * 
-*/
-void init_dog(struct dog *d, char *name, float age, char *owner) 
+ * init_dog - initialization of dog
+ * @d:pointer to dog
+ * @name:dogs name
+ * @age:dogs age
+ * @owner:dogs owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
