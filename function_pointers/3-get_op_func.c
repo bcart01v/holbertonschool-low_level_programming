2fa92a990b8b75f4fe62075f94146b6e7a194e31
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-26 16:34:37 -0500

    More corrections

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,26 +1,30 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include "3-calc.h"
-#include "3-op_functions.c"
 
 /**
- * get_op_func - Get the op func object
- * @s: character
- * Return: int(*)(int, int)
+ * get_op_func -  the function that selects the correct function
+ * to perform the operation asked by the user.
+ * @s: An input char pointer operator passed as argument
+ * Return: a pointer to the function that corresponds to the
+ * operator given as a parameter.
  */
-
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i = 0;
-while (ops[i].op != NULL && *(ops[i].op) != *s)
-i++;
-return (ops[i].f);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (*(ops[i]).op == *s && *(s + 1) == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
