5a317b31d08a54b85c7dfa4826e7132984c9b872
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-26 17:31:37 -0500

    Updated List

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -7,19 +7,20 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
+	
 	va_list args;
 	va_start(args, n);
-	
+
 	if (n == 0)
 	{
 		return (0);
 	}
-	
+
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
 		va_end(args);
-		return(sum);
+		return (sum);
 }
 
