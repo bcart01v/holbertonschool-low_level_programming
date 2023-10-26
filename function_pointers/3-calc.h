e3c15031f50bfbac99a1fce0cb961dba69a33f1a
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-26 16:43:32 -0500

    My error was my header

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,7 +1,5 @@
-#ifdef CALC_3_H
-#define CALC_3_H
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef CALC_H
+#define CALC_H
 
 /**
  * struct op - Struct op
@@ -11,16 +9,14 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
-/**
- * Below are the operators requested
-*/
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int(*op_func)(int,int);
+int (*get_op_func(char *s))(int, int);
 #endif
