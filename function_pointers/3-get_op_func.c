867fb9b142454071cb8f9ee6efa99511a07cebd9
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-26 16:22:19 -0500

    Corrections

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,34 +1,25 @@
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include "3-calc.h"
 
 /**
- * get_op_func- get thefunctions
- * @s:pointer
- * Return:NULL
+ * get_op_func - Get the op func object
+ * @s: character
+ * Return: int(*)(int, int)
  */
+
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-
-	int i = 0;
-
-	while (ops[i].op)
-	{
-		if (*(ops[i].op) == *s && s[i] == '\0')
-		{
-			return (ops[i].f);
-		}
-		i++;
-	}
-	printf("error\n");
-	exit(99);
-	return (NULL);
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (ops[i].op != NULL && *(ops[i].op) != *s)
+i++;
+return (ops[i].f);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,44 +1,37 @@
-#include "3-calc.h"
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#include "3-calc.h"
+#include "function_pointers.h"
 /**
- * main - this the starting point for arithmeti operation
- * @argc:interger rep
- * @argv:array of pointer
- * Return: 0 success
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0.
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int (*op_func)(int, int);
-
-	int num1;
-	int num2;
-	char operator;
-	int result;
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	num1 = atoi(argv[1]);
-	operator = argv[2][0];
-	num2 = atoi(argv[3]);
-
-	op_func = get_op_func(operator);
-
-
-	if (op_func == NULL)
-	{
-		printf("error\n");
-		exit(99);
-	}
-
-	result = op_func(num1, num2);
-
-	printf("%d\n", result);
-	return (0);
+int num1, num2;
+char *op;
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
+num1 = atoi(argv[1]);
+op = argv[2];
+num2 = atoi(argv[3]);
+if (get_op_func(op) == NULL || op[1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+if ((*op == '/' && num2 == 0) ||
+(*op == '%' && num2 == 0))
+{
+printf("Error\n");
+exit(100);
+}
+printf("%d\n", get_op_func(op)(num1, num2));
+return (0);
 }
