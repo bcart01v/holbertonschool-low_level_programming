c73da729d4c5cfa39b3d596b907ad8498d1727e1
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-26 12:16:57 -0500

    Project 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates an array
+ * @array: The array we're passed
+ * @action: the action I suppose
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int counter = 0;
+	int length = size;
+	if (array == NULL && action == NULL) 
+	{
+		/* we have nothing, we return nothing.*/
+		return;
+	}
+	
+	while (counter < length)
+	{
+		action(array[counter]);
+		counter++;
+	}
+	/* size is the size of the array, action is the pointer we need to use*/
+}
