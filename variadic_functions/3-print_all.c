c3247dfdf650f66bd008dead625162376feaa9da
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-27 11:40:55 -0500

    Betty

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,7 +10,6 @@ void print_all(const char * const format, ...)
 
 int i = 0;
 char *str, *sep = "";
-
 va_list list;
 
 va_start(list, format);
@@ -21,11 +20,9 @@ if (format)
 	{
 		switch (format[i])
 		{
-			/* Char */
 			case 'c':
 			printf("%s%c", sep, va_arg(list, int));
 			break;
-			/* Integer */
 			case 'i':
 			printf("%s%d", sep, va_arg(list, int));
 			break;
@@ -33,7 +30,6 @@ if (format)
 			case 'f':
 			printf("%s%f", sep, va_arg(list, double));
 			break;
-			/* char */
 			case 's':
 			str = va_arg(list, char *);
 			if (!str)
