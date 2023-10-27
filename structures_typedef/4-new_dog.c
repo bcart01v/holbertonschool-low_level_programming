51ab2a7c940366d459ae9cb1fb4ca8f6604e608f
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-27 09:24:47 -0500

    Correction

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -28,7 +28,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	new_dog->age = age;
-	new_dog->owner = _strdup(owner);
+	new_dog->owner = strdup(owner);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
