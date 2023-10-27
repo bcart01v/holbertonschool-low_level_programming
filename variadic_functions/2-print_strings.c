70c9133f94124a7bffd804b5415a6aa3178140f9
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-27 10:37:03 -0500

    I think this is halfway working

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
@@ -11,5 +12,21 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	
+
+	unsigned int i;
+
+	va_list(arg);
+
+	va_start(arg, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(arg, char *));
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(arg);
+	printf("\n");
 }
