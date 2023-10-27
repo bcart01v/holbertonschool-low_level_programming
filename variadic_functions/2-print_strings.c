4d1b3317b3f9d649f7720016bb98d9a92a7b9815
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-27 10:46:12 -0500

    I think this will work

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,7 +21,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
+		if (va_arg(arg, char*) == NULL )
+		{
+			printf("(nil)");
+		}
+		else 
+		{
 		printf("%s", va_arg(arg, char *));
+		}
 		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
