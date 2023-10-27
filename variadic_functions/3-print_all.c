7e62c18739af0ed32734339001b65c4d8a2397d4
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-27 11:32:59 -0500

    I think this will work

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * 
+ * 
+ * 
+*/
+
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str, *sep;
+
+	va_list list;
+	va_start (list, format);
+
+	if (format)
+	{
+		while (format[i])
+	{
+		switch (format[i])
+		{
+			/* Char */
+			case 'c':
+			printf("%s%c", sep, va_arg(list, int));
+			break;
+			
+			/* Integer */
+			case 'i':
+			printf("%s%d",sep, va_arg(list,int));
+			break;
+			
+			/* float */
+			case 'f':
+			printf("%s%f",sep, va_arg(list,double));
+			break;
+			
+			/* char */
+			case 's':
+			str = va_arg(list, char *);
+			
+			if (!str)
+				str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
+				default:
+			i++;
+			continue;
+		}
+		sep = ", ";
+		i++;
+	}
+	}
+	printf("\n");
+	va_end(list);
+}
