0e2011793bc056a54e6bdfa8ad2bce5e4036684c
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-10-27 09:12:58 -0500

    Betty Fix

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,8 +13,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	unsigned int i;
 	va_list(arg);
+	
 	va_start(arg, n);
-
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(arg, int));
