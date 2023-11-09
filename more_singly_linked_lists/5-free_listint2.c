50cf52bd409df2ef339dd713314bf129b892c49c
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-09 13:26:41 -0600

    Test

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,12 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *releaseme;
-/* It's a pointer to a pointer, we have to dereference */
-/* original to free properly */
+
 	if (head == NULL){
 		return;
 	}
+/* It's a pointer to a pointer, we have to dereference */
+/* original to free properly */
 	while (*head != NULL)
 	{
 		releaseme = *head;
--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -23,12 +23,13 @@ typedef struct listint_t
 	struct listint_t *next;
 } listint_t;
 
-void free_listint(listint_t *head);
-void free_listint2(listint_t **head);
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
 
 
 
