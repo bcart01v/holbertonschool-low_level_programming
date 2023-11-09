2dfb5c1fe14b00a01a427dd12ff1ae5e7c6a6520
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-08 20:45:40 -0600

    Betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,10 +1,16 @@
 #include "lists.h"
 
-/***
- * 
- * 
- * 
-*/
+/**
+ * add_node - Adds a new node at the beginning of a linked list.
+ * @head: A double pointer to the head of the list, which will be modified
+ * to point to the newly added node if the addition is successful.
+ * @str: A pointer to the string to be duplicated and stored in the new node.
+ * Return: A pointer to the newly added node, which is now the head of
+ * the list.
+ * If the function fails to create a new node (e.g., due to memory
+ * allocation failure), NULL is returned.
+ */
+
 
 list_t *add_node(list_t **head, const char *str)
 {
