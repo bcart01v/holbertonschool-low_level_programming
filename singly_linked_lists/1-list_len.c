43a75464d4e4c5c2bcf706d69f4837c5f841bff0
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-08 20:39:10 -0600

    Betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,18 +2,17 @@
 
 /**
  * list_len - Calculates the number of elements in a linked list.
- * @head: A pointer to the first node of the list.
+ * @h: A pointer to the first node of the list.
  * Return: The number of nodes in the list starting from @head.
- * If the list is empty, or if @head is NULL, the function returns 0.
  */
 
 
 size_t list_len(const list_t *h)
 {
 	const list_t *TempPointer = h;
-	size_t counter = 0; 
+	size_t counter = 0;
 
-	if (TempPointer == NULL) 
+	if (TempPointer == NULL)
 	{
 	return (0);
 	}
