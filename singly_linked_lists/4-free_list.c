6ad9ddd2576f46151d402cbb6f534bc11f4078cf
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-08 20:52:36 -0600

    Betty

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,14 +1,16 @@
 #include "lists.h"
 
 /**
- * 
- * 
- * 
-*/
+ * free_list - Frees a linked list.
+ * @head: A pointer to the start of the list.
+ *
+ * This function releases all the memory used by a linked list,
+ */
 
 void free_list(list_t *head)
 {
 	list_t *freedom;
+	
 	while (head !=NULL)
 	{
 		freedom = head->next;
