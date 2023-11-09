7c202eb745320e511edfd7f1dccdd775972ae03e
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-09 17:34:11 -0600

    Summing up a list!

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - We're summing up a list!
+ * @head: The list passed to us to be summed
+ * Return: The sum of the list.
+*/
+
+int sum_listint(listint_t *head)
+{
+	int thisvalue = 0;
+	int total = 0;
+
+	while(head != NULL)
+	{
+		thisvalue = head->n;
+		total = (total + thisvalue);
+		head = head->next;
+	}
+
+return (total);
+}
