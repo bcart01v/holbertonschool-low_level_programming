541f885cce1a15faa5e7109b8d75ebdad511bdef
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-08 20:51:09 -0600

    Betty2

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -16,13 +16,14 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	char *tempstr = strdup(str);
 	list_t *new_entry = malloc(sizeof(list_t));
-	
+
 	if (tempstr == NULL)
 	{
 		return (NULL);
 	}
 
-	if (new_entry == NULL) {
+	if (new_entry == NULL)
+	{
 		free(tempstr);
 		return (NULL);
 	}
@@ -31,18 +32,19 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_entry->len = strlen(tempstr);
 	new_entry->next = NULL;
 
-	if (*head == NULL) 
+	if (*head == NULL)
 	{
 		*head = new_entry;
 	}
 	else
 	{
 		list_t *current = *head;
-		while (current->next !=NULL) 
+
+		while (current->next != NULL)
 		{
 			current = current->next;
 		}
 		current->next = new_entry;
 	}
-	return new_entry;
+	return (new_entry);
 }
