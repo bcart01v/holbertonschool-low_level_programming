5f739cc0a5da8f472dccc9d6e32c84559cd40a8f
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-09 13:09:41 -0600

    Trying to fix memory issue

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -10,6 +10,9 @@ void free_listint2(listint_t **head)
 	listint_t *releaseme;
 /* It's a pointer to a pointer, we have to dereference */
 /* original to free properly */
+	if (head == NULL){
+		return;
+	}
 	while (*head != NULL)
 	{
 		releaseme = *head;
