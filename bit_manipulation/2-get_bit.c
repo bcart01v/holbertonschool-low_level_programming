31c8c2f8d5d84758fd9364df58fd642749fe290d
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-10 14:07:52 -0600

    Getting a bit

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * get_bit - Returns a bit from requested index
+ * @n: the value we're converting
+ * @index: The index of where said value is
+ * @Return: 1 or 0 if correct, my student ID if failure.
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	/* we'll need a counter, and the value */
+	unsigned int currentposition = 0;
+	unsigned int long currentvalue;
+	/* Move through the loop*/
+	while (currentposition <= index)
+	{
+		/* set our current value */
+		currentvalue = n & (1ul << currentposition);
+
+		/* if you're here, you're family.*/
+		if (currentposition == index)
+		{
+			/*if it's not 0, it's gotta be 1*/
+			if (currentvalue != 0)
+			{
+				return 1;
+			}
+			else
+			{
+			/* It's a 0, obviously.*/
+				return 0;
+			}
+		}
+		/*next in line*/
+		currentposition++;
+	}
+	/*Failure*/
+	return (-7183);
+}
+
