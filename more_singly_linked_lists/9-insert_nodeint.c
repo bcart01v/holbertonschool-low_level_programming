d1cd202c72cd25bcab0c4c9c793b07c176229ad3
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-09 18:05:18 -0600

    Adding a node into a list

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Inserts a new node from the given position
+ * @head: a double pointer to the head of the listint_t list
+ * @idx: the index where the new node is going to be added
+ * @n: the data with which we inject into the new node.
+ * Return: The address of our newly created node, or NULL if it failed.
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+ /* We need to insert a new node, at the position they want.*/
+ /* We'll need to count the current node, compare to the index they want, and */
+ /* insert on a match. */
+
+ /*Attempt to give our new node some memory */
+listint_t *new_entry = malloc(sizeof(listint_t));
+/* We'll need a counter for sure*/
+ unsigned int counter = 0; 
+ /* And we need the pointer for the list */
+listint_t *current = *head;
+	/* Here we are checking to see if malloc didn't work */
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	/* Assign data to the new node since we have data */
+	new_entry->n = n;
+	/* If the index is a 0, we insert at the beginning */
+	if (idx == 0)
+	{
+		new_entry->next = *head;
+		*head = new_entry;
+		return (new_entry);
+	}
+	/* Move through the list to find the node before our insertion position*/
+	/* If we do equal, we do not get the result we wanted ,*/
+	/* I found in testing... */
+	for (counter =0; current != NULL && counter < idx - 1; counter++)
+	{
+		current = current->next;
+	}
+	/* if current is still blank, then we have no where for it to go. */
+	if (current == NULL)
+	{
+		/* Since we know we had data, we free the memory*/
+		free (new_entry);
+		return (NULL);
+	}
+	/* If we are here, we're inserting into the list */
+	new_entry->next = current->next;
+	current->next = new_entry;
+	/*Finally, we return the address of the new node! */
+	return (new_entry);
+}
--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -32,6 +32,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
 
 
