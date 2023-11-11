0d3a9f533573cc8391dab63bbe3a41efaf922715
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-11 01:06:01 -0600

    Finally got it

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * 
- * 
- * 
+ * set_bit - sets a bit from requested index
+ * @n: pointer to uli, the number which will be set.
+ * @index: the specific bit we will set.
+ * Return: -1 if failure, otherwise 1
 */
 
 
@@ -11,26 +12,21 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 /* we'll need a counter, and the value */
 
-	unsigned int currentposition = 0;
-
 	if (index >= sizeof(*n) * 8)
 	{
 	/* Added this because on check was failing, */
 	/* did not account for out of bounds scenerios.*/
 		return (-1);
 	}
+	/* it turns out, I don't need a loop when I have an */
+	/* index.. that means I could simplify 2-get_bit probably */
 
-	/* Move through the loop*/
-	while (currentposition <= index)
-	{
-		/* if you're here, you're family.*/
-		if (currentposition == index)
-		{
-			 *n |= 1 << index;
-		}
-		/*next in line*/
-		currentposition++;
-	}
-	/*Failure*/
-	return (-7183);
+	/* Set the bit at the requested index */
+	/* 1UL << index moves 1 to the left by value of index */
+	/* then OR it with *n to set the bit at said index. */
+
+	*n |= (1UL << index);
+	
+	/*if it worked, we return 1 as requested */
+	return (1);
 }
