4a2c4a2528614b6c3adb417bcfab5ad1b9e4624e
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-13 17:05:21 -0600

    Documentation Correction

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
  * read_textfile - pass on a text file and this function will print it.
  * @filename: The file we're printing
  * @letters: The number of letters we're printing.
- * Return - a 0 if failed, the count if no fail.
+ * Return: a 0 if failed, the count if no fail.
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
