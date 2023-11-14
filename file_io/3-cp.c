56836b0c07f52590bd64007583eecf53fc719870
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-13 19:34:29 -0600

    project 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,72 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entrypoint
+ * @argc: Number of arguments passed into program
+ * @argv: The variables passed through the argument.
+ * @Return: 0 On success, all others are errors;
+*/
+
+void close_file(int fd) 
+{
+	if (close(fd) == -1) 
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, read_bytes, write_bytes;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		close_file(file_from);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((read_bytes = read(file_from, buffer, 1024)) > 0)
+	{
+		write_bytes = write(file_to, buffer, read_bytes);
+		if (write_bytes != read_bytes)
+		{
+			close_file(file_from);
+			close_file(file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (read_bytes == -1)
+	{
+		close_file(file_from);
+		close_file(file_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	close_file(file_from);
+	close_file(file_to);
+	return (0);
+}
