a1728df69f6c504e4feb33db93e104126ef36743
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-13 19:41:29 -0600

    Shrinking Down

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,25 +1,22 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-
+/**
+ * close_file - Closes a file
+ * @fd: file to close.
+*/
+void close_file(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
 /**
  * main - Entrypoint
  * @argc: Number of arguments passed into program
  * @argv: The variables passed through the argument.
  * @Return: 0 On success, all others are errors;
 */
-
-void close_file(int fd) 
-{
-	if (close(fd) == -1) 
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
-
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, read_bytes, write_bytes;
@@ -30,14 +27,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
@@ -45,7 +40,6 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	while ((read_bytes = read(file_from, buffer, 1024)) > 0)
 	{
 		write_bytes = write(file_to, buffer, read_bytes);
@@ -57,7 +51,6 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
 	if (read_bytes == -1)
 	{
 		close_file(file_from);
@@ -65,7 +58,6 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	close_file(file_from);
 	close_file(file_to);
 	return (0);
