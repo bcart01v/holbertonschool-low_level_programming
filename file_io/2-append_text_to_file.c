222f694455f6bc49db1eb20d4bf26444b36d8d13
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-13 19:22:47 -0600

    Docs

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * append_text_to_fie - appends text passed into file.
+ * append_text_to_file - appends text passed into file.
  * @filename: The file
- * @text_contents: The contents to write.
+ * @text_content: The contents to write.
  * Return: 1 If it works, -1 if it doesn't.
 */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-/* Take the text we get, append it to file. Easy right? */
-/* see previous programs for documentation, I'm tired. */
+
 	int thefile;
 	ssize_t written = 0, length = 0;
+
 	if (filename == NULL)
 	{
 		return (-1);
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		while(text_content[length])
+		while (text_content[length])
 		length++;
 
 		written = write(thefile, text_content, length);
@@ -37,5 +37,4 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	close(thefile);
 	return (1);
-
 }
