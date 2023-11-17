7a6f676b3f370d4f0d5aaea2f1eef7eb6678140b
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-16 18:54:11 -0600

    Couldn't be that simple

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,7 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table); 
+		return (NULL);
 
-
+	}
 	return (table);
 }
