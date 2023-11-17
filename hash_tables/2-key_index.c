56e5e3bbceaf47cbe000ec0544328e190d99c56d
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-17 14:48:31 -0600

    Totally wrote a hash function algorithm

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * 
+ * 
+ * 
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+/* Key is the Key */
+/* Size is the size of the array in the hash table	*/
+
+
+}
