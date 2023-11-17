122a7846e2a845c177c51479adf21db0be1e5640
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-16 19:01:40 -0600

    Betty Fixes

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * has_table_create - Creates a hash table.
+ * hash_table_create - Creates a hash table.
  * @size: the size they want the hash table.
  * Return: Pointer to new table, or Null.
 */
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
+
 	if (table == NULL)
 		return (NULL);
 
@@ -16,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 	{
-		free(table); 
+		free(table);
 		return (NULL);
 
 	}
