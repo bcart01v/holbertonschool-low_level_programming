ce9bce1b570185cd74cf5129cc4699ee826840f6
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-17 15:23:35 -0600

    If you want space, I'll give you space.

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -2,19 +2,28 @@
 
 /**
  * key_index - Returns the key index.
- * @key: Pointer of key 
+ * @key: Pointer of key
  * @size: Size of the array.
  * Return: Returns the hash size
 */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+
 /* Key is the Key */
+
 /* Size is the size of the array in the hash table	*/
-	if (key!= NULL)
+
+	if (key != NULL)
+
 	{
-	long unsigned int hash = hash_djb2(key);	
+
+	unsigned long int hash = hash_djb2(key);
+
 	return (hash % size);
+
 	}
+
 	return (0);
+
 }
