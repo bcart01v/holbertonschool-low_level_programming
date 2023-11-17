f45e3994ec791e881a08fc5c69b5f94cf74eaeae
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-17 14:59:29 -0600

    I think this will work

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,15 +1,19 @@
 #include "hash_tables.h"
 
 /**
- * 
- * 
- * 
+ * key_index - Returns the key index.
+ * @key: Pointer of key 
+ * @size: Size of the array.
+ * Return: Returns the hash size
 */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 /* Key is the Key */
 /* Size is the size of the array in the hash table	*/
-
-
+	if (key!= NULL)
+	{
+	unsigned long int hash = hash_djb2(key);
+	return (hash % size);
+	}
 }
