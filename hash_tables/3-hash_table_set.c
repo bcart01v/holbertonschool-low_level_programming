3a0afd0615cb3381f4f87ca36fdfc4d60d111a76
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-17 19:53:28 -0600

    Trying to simplify

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,35 +13,26 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 	hash_node_t *current;
-
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
-	
 	if (new_node == NULL)
 		return (0);
-
 	new_node->key = strdup(key);
-	
 	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
-
 	new_node->value = strdup(value);
-	
 	if (new_node->value == NULL)
 	{
 		free(new_node->key);
 		free(new_node);
 		return (0);
 	}
-
 	current = ht->array[index];
-	
 	while (current != NULL)
 	{
 		if (strcmp(key, current->key) == 0)
@@ -54,7 +45,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		current = current->next;
 		}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
