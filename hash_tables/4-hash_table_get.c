78c0d57cdbe100be6287ee72e8872e550553ba4e
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-17 21:02:32 -0600

    Indention Correction

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -12,10 +12,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *node;
 	unsigned long int index;
 
-if (ht == NULL || key == NULL)
-	{
-		return (NULL);
-	}
+	if (ht == NULL || key == NULL)
+		{
+			return (NULL);
+		}
 
 	index = key_index((const unsigned char *)key, ht->size);
 
