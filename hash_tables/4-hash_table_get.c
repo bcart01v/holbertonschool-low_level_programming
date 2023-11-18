e7027e54af9541d510fc6cbfe49cb0fe9c160685
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-17 21:01:45 -0600

    Getting Hash

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with passed key.
+ * @ht: Pointer to the table.
+ * @key: The key we use to get the value
+ * Return: Either the value, or NULL
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	for (node = ht->array[index]; node != NULL; node = node->next)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node->value);
+		}
+	}
+
+	return (NULL);
+}
