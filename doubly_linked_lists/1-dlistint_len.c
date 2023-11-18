129d9640466f13c5ad1490fd54ba4b542c3ce8bd
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-18 01:41:23 -0600

    Made a very crucial error..

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * 
- * 
- * 
+ * dlistint_len - Counts the number of elements in a doubly linked list.
+ * @h: Pointer to the head of the list
+ * Return: The number of elements in the list.
 */
 
 size_t dlistint_len(const dlistint_t *h)
@@ -13,6 +13,7 @@ size_t dlistint_len(const dlistint_t *h)
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
+		h = h->next;
 		counter++;
 	}
 	return (counter);
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -26,4 +26,5 @@ size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 
 
+
 #endif
