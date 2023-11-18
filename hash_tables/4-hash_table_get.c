903d855b9490ca000924752211d2d3645310013b
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-17 21:08:04 -0600

    Mayve that will work

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -13,9 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL)
-		{
-			return (NULL);
-		}
+	return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
