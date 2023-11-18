9ebbcb67cd81bbfdb2a309f87751332bc5cbd318
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-11-17 19:54:33 -0600

    WHITESPACE

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 	hash_node_t *current;
-	
+
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
